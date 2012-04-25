3ab533e54d9598e5048f996baccaee00ad952920
feesh <feeshsauce@gmail.com>
2012-04-25 16:09:45 -0700

    Added Diazo dependency.

--- ucdavis.rebrand/setup.py
+++ ucdavis.rebrand/setup.py
@@ -25,6 +25,7 @@ setup(name='ucdavis.rebrand',
       zip_safe=False,
       install_requires=[
           'setuptools',
+          'plone.app.theming',
           # -*- Extra requirements: -*-
       ],
       entry_points="""
