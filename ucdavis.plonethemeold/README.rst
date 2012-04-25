7ee8be38b5f04807c71c27986d6fe9ae4f73166f
Trish Ang <feeshsauce@gmail.com>
2012-04-25 14:15:47 -0700

    Added the folders!

---
+++ ucdavis.plonethemeold/README.rst
@@ -0,0 +1,6 @@
+Introduction
+============
+
+Simple skeleton for a Diazo (plone.app.theming) add on with plugins.
+
+This product was created with ZopeSkel, and likely contains traces of nuts.
---
+++ ucdavis.plonethemeold/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='ucdavis.plonetheme',
+      version=version,
+      description="Diazo Template",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ucdavis'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.theming',
+          'plone.app.themingplugins',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
